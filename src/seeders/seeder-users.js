3b1738d5bcf0a5667b66b369ea0d95753d444f86
ThienNghia1 <thiennghia142001@gmail.com>
2023-05-17 22:36:28 +0700

    tao models

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -1,11 +1,18 @@
 'use strict';
 
 module.exports = {
+    
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      firstName: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'nghia',
+      lastName: 'thien',
+      address: 'long an',
+      gender: 1,
+      typeRole:'ROLE',
+      keyRole:'R1',
+      
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
